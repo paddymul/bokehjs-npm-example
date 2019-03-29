9db9739458777154fdc52a9522b019b5e92756cf
Paddy Mullen <paddy@paddymullen.com>
2019-03-28 19:27:28 -0600

    stripped down example of using bokehjs node module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Bokeh from 'bokehjs';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const BokehExample = (el) => {
+
+    var plt = Bokeh.api.Plotting;
+
+    // set up some data
+    var M = 100;
+    var xx = [];
+    var yy = [];
+    var colors = [];
+    var radii = [];
+    for (var y = 0; y <= M; y += 4) {
+        for (var x = 0; x <= M; x += 4) {
+            xx.push(x);
+            yy.push(y);
+            colors.push(plt.color(50+2*x, 30+2*y, 150));
+            radii.push(Math.random() * 0.4 + 1.7);
+        }
+    }
+    // create a data source
+    var source = new Bokeh.api.ColumnDataSource({
+        data: { x: xx, y: yy, radius: radii, colors: colors }
+    });
+
+    // make the plot and add some tools
+    var tools = "pan,crosshair,wheel_zoom,box_zoom,reset,save";
+    var p = plt.figure({ title: "Colorful Scatter", tools: tools });
+
+    // call the circle glyph method to add some circle glyphs
+    var circles = p.circle({ field: "x" }, { field: "y" }, {
+        source: source,
+        radius: radii,
+        fill_color: colors,
+        fill_alpha: 0.6,
+        line_color: null
+    });
+
+    var doc = new Bokeh.api.Document();
+    doc.add_root(p);
+    Bokeh.embed.add_document_standalone(doc, el); 
+}
+
+const onLoad = () => {
+    
+    BokehExample(document.getElementById('root'));
+}
+
+document.addEventListener('DOMContentLoaded', onLoad, false);
+
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+ serviceWorker.unregister();
